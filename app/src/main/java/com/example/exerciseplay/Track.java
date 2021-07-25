b54c0737c0e013149e0aff633944b359fb661f12
tabularasa98 <50341539+tabularasa98@users.noreply.github.com>
2021-07-25 14:58:57 -0400

    Initial commit

---
+++ app/src/main/java/com/example/exerciseplay/Track.java
@@ -0,0 +1,22 @@
+package com.example.exerciseplay;
+
+public class Track {
+    private int id;
+    private boolean readable;
+    private String title;
+    private String title_short;
+    private String title_version;
+    private boolean unseen;
+    private String isrc;
+    private String link;
+    private String share;
+    private int duration;
+    private int track_position;
+    private int disc_number;
+    private int rank;
+    private String date;
+    private boolean explicit_lyrics;
+    private int explicit_content_lyrics;
+    private int explicit_content_cover;
+    
+}
